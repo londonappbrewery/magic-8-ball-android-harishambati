5c3f13cbe144fff049d8b4c533f132e4d6e04671
harishambati <harishambati97@gmail.com>
2018-03-01 16:52:16 +0530

    Gradle Plugin & Build Tool Update

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       final ImageView image =findViewById(R.id.cap);
+       final int[] array=new int[]{R.drawable.antmanlogo,
+                R.drawable.bpanther,
+                R.drawable.cap,
+                R.drawable.superlogo,
+                R.drawable.wolverine,
+                R.drawable.wonderwoman,
+                R.drawable.flash};
+        Button noobbutton =findViewById(R.id.noobbutton);
+
+        noobbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random rndnmbr = new Random();
+                int harish =rndnmbr.nextInt(5);
+                image.setImageResource(array[harish]);
+            }
+        });
+
+
     }
+
+
+
+
+
+
 }
+
+
+
+
